8e8b08acb064dff8e8704c7c718f5942607d1146
gybran <charistomarcgybran@students.undip.ac.id>
2025-02-23 12:15:47 +0700

    init: belajar spring aop

---
+++ belajar-spring-aop/src/test/java/programmer/zaman/now/aop/BelajarSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
