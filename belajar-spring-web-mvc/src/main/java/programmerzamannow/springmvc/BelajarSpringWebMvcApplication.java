1de90a7c1ed9f895a1674f1cbe9af207448002e5
Charisto Marc Gybran(Charisto Marc Gybran)[charistomarc] <charistomarc@LGENSOL.GLOBAL>
2025-03-19 14:55:50 +0700

    progress:spring-web-mvc-done

--- belajar-spring-web-mvc/src/main/java/programmerzamannow/springmvc/BelajarSpringWebMvcApplication.java
+++ belajar-spring-web-mvc/src/main/java/programmerzamannow/springmvc/BelajarSpringWebMvcApplication.java
@@ -2,12 +2,27 @@ package programmerzamannow.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
+@ServletComponentScan
 public class BelajarSpringWebMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BelajarSpringWebMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarSpringWebMvcApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .connectTimeout(Duration.ofSeconds(2L))
+                .readTimeout(Duration.ofSeconds(2L))
+                .build();
+    }
 
 }
